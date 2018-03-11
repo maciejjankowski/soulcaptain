94fccb1d5e7026cd4eba26509dfe91d55c83ab0f
Wiktor <roktiw@gmail.com>
2018-03-11 22:35:00 +0100

    ejs przenosiny

--- routes.js
+++ routes.js
@@ -1,17 +1,37 @@
-module.exports = function _defineRoutes(deps){
-  if (typeof deps['app'] === 'undefined'){
-    throw new Error('requires app dependency');
+module.exports = function _defineRoutes(deps) {
+  if (typeof deps.app === "undefined") {
+    throw new Error("requires app dependency");
   } else {
     var app = deps.app;
   }
-  
-  app.get('/', (req, res) => {
-    res.render('index');
+
+  app.get("/", (req, res) => {
+    res.render("index");
   });
-  
-   app.get('/deck.html', (req, res) => {
-    res.render('deck.html');
+
+  app.get("/deck.html", (req, res) => {
+    res.render("deck.html");
   });
-  
-  return app
-}
+
+  app.get("/deckcard.html", (req, res) => {
+    res.render("deckcard.html");
+  });
+
+  app.get("/habits.html", (req, res) => {
+    res.render("habits.html");
+  });
+
+  app.get("/login.html", (req, res) => {
+    res.render("/login.html");
+  });
+
+  app.get("/signup.html", (req, res) => {
+    res.render("/signup.html");
+  });
+
+  app.get("/admin.html", (req, res) => {
+    res.render("/admin.html");
+  });
+
+  return app;
+};
