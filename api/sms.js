be88f3e6ed447dc0cb94c5e2984424ae6f45e6f6
Maciej Jankowski <maciej.artur.jankowski@gmail.com>
2018-03-18 15:37:40 +0100

    pszontki

---
+++ api/sms.js
@@ -0,0 +1,29 @@
+var SMSAPI = require('smsapi'),
+    smsapi = new SMSAPI();
+
+smsapi.authentication
+    .loginHashed(process.env.SMS_USER, process.env.SMS_PASS);
+   
+
+function sendMessage(from, to, text, displayResult, displayError){
+   // implement queueing / state saving
+  
+    return smsapi.message
+        .sms()
+        .from(from)
+        .to(to)
+        .message(text)
+        .execute()
+        .then(displayResult)
+        .catch(displayError);
+  // return Promise
+}
+
+function displayResult(result){
+    console.log(result);
+}
+
+function displayError(err){
+    console.error(err);
+}
+module.exports = sendMessage;
