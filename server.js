e122b535083b5fd197ef4e2886446885debdc188
Maciej Jankowski <maciej.artur.jankowski@gmail.com>
2018-03-18 15:37:01 +0100

    pszontki

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
-const mail = require('./email.js');
-const bcrypt = require('bcrypt');
-const sms = require('./sms.js');
+// const mail = require('./api/email.js');
+// const bcrypt = require('bcrypt');
+// const sms = require('./api/sms.js');
 
 const mongoConnString = `mongodb://${process.env.DBUSER}:${process.env.DBPASS}@${process.env.DBHOST}/${process.env.DBNAME}`;
 mongoose.connect(mongoConnString);
@@ -26,9 +26,9 @@ app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('body-parser').json());
 
 app.use(session({
-  secret: process.env.SESSIONSECRET,
-  store: new MongoStore({ mongooseConnection: mongoose.connection }),
-  resave: true, saveUninitialized: true
+	secret: process.env.SESSIONSECRET,
+	store: new MongoStore({ mongooseConnection: mongoose.connection }),
+	resave: true, saveUninitialized: true
 }));
 
 app.use(passport.initialize());
@@ -37,10 +37,10 @@ app.use(passport.session());
 // using passport: https://stackoverflow.com/questions/45381931/basics-of-passport-session-expressjs-why-do-we-need-to-serialize-and-deseriali
 const schema = require('./api/schema.js')(mongoose);
 const deps = { // dependencies object passed to every require function (poor mans dependency injection)
-  app,
-  mongoose,
-  passport,
-  isAuthenticated
+	app,
+	mongoose,
+	passport,
+	isAuthenticated
 };
 
 require('./api/passport.js')(mongoose, passport);
@@ -51,7 +51,7 @@ require('./api/user.js')(app, mongoose, passport);
 require('./routes.js')(deps);
 
 var listener = app.listen(process.env.PORT || 9000, function () {
-  console.log('SoulCaptain is listening on port ' + listener.address().port);
+	console.log('SoulCaptain is listening on port ' + listener.address().port);
 });
 
 /**
@@ -59,20 +59,20 @@ var listener = app.listen(process.env.PORT || 9000, function () {
  */
 function isAuthenticated(req, res, next){
 
-  console.log('testing for login');
-  if (req.isAuthenticated()) {
-    console.log('login ok');
-    return next();
-  } else{
+	console.log('testing for login');
+	if (req.isAuthenticated()) {
+		console.log('login ok');
+		return next();
+	} else{
 
-    console.log('missing login');
+		console.log('missing login');
 
-    if (req.headers['content-type'] === "application/json; charset=UTF-8"){
-      res.status(403).send("please log in");
-    } else {
-      res.redirect('/login.html');
-    }
-  }
+		if (req.headers['content-type'] === "application/json; charset=UTF-8"){
+			res.status(403).send("please log in");
+		} else {
+			res.redirect('/login.html');
+		}
+	}
 }
 
 app.isAuthenticated = isAuthenticated;
@@ -80,12 +80,12 @@ app.isAuthenticated = isAuthenticated;
  * Authorization Required middleware.
  */
 exports.isAuthorized = (req, res, next) => {
-  const provider = req.path.split('/').slice(-1)[0];
-  const token = req.user.tokens.find(token => token.kind === provider);
-  if (token) {
-    next();
-  } else {
-    res.redirect(`/auth/${provider}`);
-  }
+	const provider = req.path.split('/').slice(-1)[0];
+	const token = req.user.tokens.find(token => token.kind === provider);
+	if (token) {
+		next();
+	} else {
+		res.redirect(`/auth/${provider}`);
+	}
 };
 
