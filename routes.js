b90f7310412ed33f3eea3ad75f74d87c36b7401a
Maciej Jankowski <maciej.artur.jankowski@gmail.com>
2018-03-18 14:29:10 +0100

    fixy jakieś

--- routes.js
+++ routes.js
@@ -1,45 +1,44 @@
 module.exports = function _defineRoutes(deps) {
-  var app;
-  if (typeof deps.app === "undefined") {
-    throw new Error("requires app dependency");
-  } else {
-    app = deps.app;
-  }
+	var app;
+	if (typeof deps.app === 'undefined') {
+		throw new Error('requires app dependency');
+	} else {
+		app = deps.app;
+	}
 
-  app.get("/", (req, res) => {
-    let templateData = {title : "XYZ"};
-    res.render("index", templateData);
-  });
+	app.get('/', (req, res) => {
+		let templateData = {title : 'XYZ'};
+		res.render('index', templateData);
+	}); 
+	app.get('/deck.html', (req, res) => {
+		let templateData = {title : 'XYZ'};
+		res.render('deck.html', templateData);
+	});
 
-  app.get("/deck.html", (req, res) => {
-    let templateData = {title : "XYZ"};
-    res.render("deck.html", templateData);
-  });
+	app.get('/deckcard.html', (req, res) => {
+		let templateData = {title : '🃏 Single Card'};
+		res.render('deckcard.html', templateData);
+	});
 
-  app.get("/deckcard.html", (req, res) => {
-    let templateData = {title : "🃏 Single Card"};
-    res.render("deckcard.html", templateData);
-  });
+	app.get('/habits.html', (req, res) => {
+		let templateData = {title : 'XYZ'};
+		res.render('habits.html', templateData);
+	});
 
-  app.get("/habits.html", (req, res) => {
-    let templateData = {title : "XYZ"};
-    res.render("habits.html", templateData);
-  });
+	app.get('/signup.html', (req, res) => {
+		let templateData = {title : 'XYZ'};
+		res.render('signup.html', templateData);
+	});
 
-  app.get("/signup.html", (req, res) => {
-    let templateData = {title : "XYZ"};
-    res.render("signup.html", templateData);
-  });
+	app.get('/login.html', (req, res) => {
+		let templateData = {title : 'XYZ'};
+		res.render('login.html', templateData);
+	});
 
-  app.get("/login.html", (req, res) => {
-    let templateData = {title : "XYZ"};
-    res.render("login.html", templateData);
-  });
+	app.get('/admin.html', (req, res) => {
+		let templateData = {title : 'XYZ'};
+		res.render('admin.html', templateData);
+	});
 
-  app.get("/admin.html", (req, res) => {
-    let templateData = {title : "XYZ"};
-    res.render("admin.html", templateData);
-  });
-
-  return app;
+	return app;
 };
