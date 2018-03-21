93356f422f8436af1a023289ff2e7b51fd3d4f56
Wiktor <roktiw@gmail.com>
2018-03-21 22:45:07 +0100

    😩 user grit fix

--- routes.js
+++ routes.js
@@ -14,15 +14,14 @@ module.exports = function _defineRoutes(deps) {
 			title: "👻 Soulcaptain 🏡 Home",
 			greeting: ""
 		};
-		if (req.user && req.user.email) {
-			templateData.greeting = "Hello, 👩‍💻 ";
-		}
+		greetUser(req, templateData);
 		templateData.user = req.user;
 		res.render("index", templateData);
 	});
 
 	app.get("/deck.html", (req, res) => {
 		let templateData = { title: "🎴 Deck" };
+		greetUser(req, templateData);
 		templateData.user = req.user;
 		console.log(templateData.user, "tu zoba co siedzi");
 		res.render("deck.html", templateData);
@@ -30,11 +29,13 @@ module.exports = function _defineRoutes(deps) {
 
 	app.get("/deckcard.html", (req, res) => {
 		let templateData = { title: "🃏 Single Card" };
+		greetUser(req, templateData);
 		res.render("deckcard.html", templateData);
 	});
 
 	app.get("/deckcardadd.html", (req, res) => {
 		let templateData = { title: "📝 🃏 DeckCardAdd" };
+		greetUser(req, templateData);
 		res.render("deckcardadd.html", templateData);
 	});
 
@@ -43,29 +44,42 @@ module.exports = function _defineRoutes(deps) {
 		deps.isAuthenticated,
 		function showDeckCarousel(req, res) {
 			let templateData = { title: "🎠 🃏 DeckCardCarousel" };
+			greetUser(req, templateData);
 			res.render("deckcardcarousel.html", templateData);
 		}
 	);
 
 	app.get("/habits.html", (req, res) => {
 		let templateData = { title: "❌ DailyX" };
+		greetUser(req, templateData);
 		res.render("habits.html", templateData);
 	});
 
 	app.get("/signup.html", (req, res) => {
 		let templateData = { title: "✍️ Sign Up" };
+		greetUser(req, templateData);
 		res.render("signup.html", templateData);
 	});
 
 	app.get("/login.html", (req, res) => {
 		let templateData = { title: "✅ Login" };
+		greetUser(req, templateData);
 		res.render("login.html", templateData);
 	});
 
 	app.get("/admin.html", (req, res) => {
 		let templateData = { title: "Dis iz adminz place" };
+		greetUser(req, templateData);
 		res.render("admin.html", templateData);
 	});
 
 	return app;
 };
+
+function greetUser(req, templateData) {
+	if (req.user && req.user.email) {
+		templateData.greeting = 'Hello, 👩‍💻 ' + req.user.personalInfo[0].firstName;
+	} else {
+		templateData.greeting = 'Please' ;
+	}
+}
