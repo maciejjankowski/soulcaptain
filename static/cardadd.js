1f1c89bb46ec655279a77f11a8f5c06eab037650
Wiktor Roktiw <roktiw@gmail.com>
2018-04-15 18:37:45 +0200

    json sie robi w okienku deck card add

--- static/cardadd.js
+++ static/cardadd.js
@@ -24,14 +24,35 @@ var appForAddingCardsToBackendBeta9000 = new Vue({
                 dump(request.responseText)
             }
 
+        },
+        inputs2json: function () {
+            var out2 = {
+                "soulCardId": "",
+                "soulCardTitle": document.getElementById("soulCardTitle").value,
+                "soulCardSoulencje": [{
+                    "source": {
+                        "author": document.getElementById("author").value,
+                        "created": document.getElementById("created").value,
+                        "source": document.getElementById("source").value,
+                        "sourceLink": document.getElementById("sourceLink").value
+                    },
+                    "_id": "",
+                    "soulIdParent": document.getElementById("soulIdParent").value,
+                    "soulType": document.getElementById("soulType").value,
+                    "language": document.getElementById("language").value,
+                    "text": document.getElementById("text").value
+                }],
+                "__v": 0
+            };
+            appForAddingCardsToBackendBeta9000.payload = JSON.stringify(out2, null, 2);
         }
-
     }
 });
 
 function dump(dumpara) {
     console.log(dumpara)
 }
+
 
 
 // TODO wczytywanie danych z istniejącej karty celem ładowania do formularza
