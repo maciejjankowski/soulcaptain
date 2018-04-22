9067fecfcb5b25e332b9a7811f126137b33e4819
Maciej Jankowski <maciej.artur.jankowski@gmail.com>
2018-04-22 17:27:02 +0200

    card api, read, update, create

--- api/deck.js
+++ api/deck.js
@@ -119,28 +119,33 @@ module.exports = function (deps) {
 
 	function createCard(req, res) {
 		let deckId = req.params.deckId;
-		// TODO!!!! walidacja karty w req.body;
+		if ((!req.user || !req.user.decks) || !req.user.decks.indexOf(deckId) > -1){
+			console.log('trying to save someone elses card or not logged in');
+			res.send(403, 'NOT OK');
+			return -1;
+		}
 
 		let card = new Card(req.body); // req;
 		card.save().then((response) => {
-			console.log('created Card', response._id, response);
 			Deck.findOne({
 				_id: deckId
 			}).then(function _handleFoundDeck(foundDeck) {
 				foundDeck.cards.push(response._id);
 				foundDeck.save().then(function (result) {
 					console.log('updated deck with new card', result);
+					res.send('OK');
 				}).catch(function _handleError(err) {
 					console.log('zapis się wysrał', err);
+					res.send(400, 'NOT OK');
 				});
 			}).catch(err => {
 				console.log('createCard error in .then(function _handleFoundDeck(foundDeck)', err);
+				res.send(400, 'NOT OK');
 			});
 		}).catch(err => {
 			console.log('createCard error in card.save().then((response)', err);
-		});
-		console.log('deckId, card', deckId, card);
-		res.send('OK');
+			res.send(400, 'NOT OK');
+		});		
 	}
 
 	function updateCard(req, res){
@@ -148,18 +153,71 @@ module.exports = function (deps) {
 		if (req.method === 'GET'){
 			formCard = require('../extras/scheme-soulcard.json');
 		}
-		let cardId = req.params.cardId;
+		let cardId = formCard._id || req.params.cardId;
+		if ((!req.user || !req.user.decks) || !req.user.decks.reduce((p, c) => {
+			return p || (c.cards.findIndex((card) => card._id === cardId) > -1);
+		}, 0)){
+			console.log('trying to save someone elses card or not logged in');
+			res.send(403, 'NOT OK');
+			return -1;
+		}
+
 		Card.findOne({_id : cardId}).then(card=>{
 			Object.assign(card, formCard);
-			return card.save();
+			card.save().then(() => {
+				res.send('OK');
+			}).catch(err => {
+				console.log('update failed', err);
+				res.send(400, 'NOT OK');
+			});
 		}).catch(err => {
 			console.log('szukanie/zapis karty failed', err);
+			res.send(400, 'NOT OK');
 		});
-		res.send('OK');
+		
 	}
 
 	function updateDeck(req, res){
-		res.send('NOT OK');
+		let deckId = req.params.deckId;
+		let inputDeck = req.body;
+		if ((!req.user || !req.user.decks) || req.user.decks.indexOf(deckId) === -1){
+			console.log('trying to save someone elses card or not logged in');
+			res.send(403, 'NOT OK');
+			return -1;
+		}
+
+		Deck.findOne({
+			_id: deckId
+		}).then(function _handleFoundDeck(foundDeck) {
+			Object.assign(foundDeck, inputDeck);
+			foundDeck.save().then(function (result) {
+				console.log('updated deck with new card', result);
+				res.send('OK');
+			}).catch(function _handleError(err) {
+				console.log('zapis się wysrał', err);
+				res.send(400, 'NOT OK');
+			});
+		}).catch(err => {
+			console.log('createCard error in .then(function _handleFoundDeck(foundDeck)', err);
+			res.send(400, 'NOT OK');
+		});
+	}
+
+	function getCard(req, res){
+		let cardId = req.params.cardId;
+		// TODO allow reading public cards
+		if ((!req.user || !req.user.decks) || !req.user.decks.reduce((p, c) => {
+			return p || (c.cards.findIndex((card) => card._id === cardId) > -1);
+		}, 0)){
+			console.log('trying to read someone elses card or not logged in');
+			res.send(403, 'NOT OK');
+			return -1;
+		}
+		Card.findOne({_id : cardId}).then(card => {
+			res.send(card);
+		}).catch(err => {
+			res.send(404, 'NOT OK');
+		});
 	}
 
 	app.post('/deck/:deckId', saveDeck);
@@ -170,5 +228,5 @@ module.exports = function (deps) {
 	app.put('/card/:cardId', updateCard);
 	app.post('/card/:cardId', updateCard);
 	app.get('/card/:cardId/update', updateCard);
-	
+	app.get('/card/:cardId', getCard);
 };
