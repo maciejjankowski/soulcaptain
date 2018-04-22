bf8049dd4c71fe042800906baae80b27a6678d39
Wiktor Roktiw <roktiw@gmail.com>
2018-04-22 15:14:28 +0200

    kleki

--- api/deck.js
+++ api/deck.js
@@ -9,7 +9,9 @@ module.exports = function (deps) {
 		// let deckId = req.params.deckId;
 		let userDecks = req.user && req.user.decks; // TODO zwróci pierwszy fałsz lub ostatnia prawda
 		if (userDecks && userDecks.length) {
-			Deck.findOne({ '_id': userDecks[0] })
+			Deck.findOne({
+					'_id': userDecks[0]
+				})
 				.populate('cards')
 				.then((deck) => {
 					res.send(deck);
@@ -20,14 +22,16 @@ module.exports = function (deps) {
 	});
 
 	app.get('/decks', (req, res) => {
-		if (req.user && req.user.decks) {    
+		if (req.user && req.user.decks) {
 			let userDecks = req.user && req.user.decks;
 
 			let listOfDecks = userDecks.map(deckId => ({
 				_id: deckId
 			}));
 			console.log('listOfDecks', JSON.stringify(listOfDecks, null, 2));
-			Deck.find({ $or: listOfDecks }).then(function _handleDecks(decks) {
+			Deck.find({
+				$or: listOfDecks
+			}).then(function _handleDecks(decks) {
 				console.log('decks:', decks);
 				// TODO: nie pokazywać userowi wszystkich pól obiektu
 				res.send(decks);
@@ -54,7 +58,9 @@ module.exports = function (deps) {
 					details: err
 				});
 			} else {
-				res.send({status: 'ok'});
+				res.send({
+					status: 'ok'
+				});
 			}
 		}); // card save
 	});
@@ -69,47 +75,52 @@ module.exports = function (deps) {
 
 		console.log('this is searched', inputDeck._id);
 
-		let deck = Deck.findOne({ _id: inputDeck._id })
-				.populate({path:'cards', })
-				.then(deck => {
-			console.log('this is found', JSON.stringify(deck, null, 2));
-			console.log('this is added', JSON.stringify(inputDeck, null, 2));
-			Object.assign(deck, inputDeck);
-      
-      // read all cards and save individually
-          
-      inputDeck.cards.forEach((inputCard)=>{
-        if (deck.cards.filter((card)=>card._id === inputCard._id).length){
-          // update
-          Card.findOne().then({});
-        } else {
-          // save, getId, add Id
-          deck.cards.push(inputCard);
-          
-        }
-      });
-          
-			console.log('this gets saved', JSON.stringify(deck, null, 2 )) ;
-			// return;
-			deck.soulDeckTitle = 'XYZ';
-			deck.save().then(result => {
-				console.log(
-					'SoulCaptain saved the deck (and your soul).',
-					JSON.stringify(result, null, 2)
-				);
-				res.send('OK');
-			}).catch((err)=>{
-				console.log('save error', err);
-				res.send('BAD\n' + JSON.stringify(err, null, 2));
+		let deck = Deck.findOne({
+				_id: inputDeck._id
+			})
+			.populate({
+				path: 'cards',
+			})
+			.then(deck => {
+				console.log('this is found', JSON.stringify(deck, null, 2));
+				console.log('this is added', JSON.stringify(inputDeck, null, 2));
+				Object.assign(deck, inputDeck);
+
+				// read all cards and save individually
+
+				inputDeck.cards.forEach((inputCard) => {
+					if (deck.cards.filter((card) => card._id === inputCard._id).length) {
+						// update
+						console.log('if dla funkcji deck.cards.filter');
+						Card.findOne().then({});
+					} else {
+						// save, getId, add Id
+						deck.cards.push(inputCard);
+
+					}
+				});
+
+				console.log('this gets saved', JSON.stringify(deck, null, 2));
+				// return;
+				deck.soulDeckTitle = 'XYZ';
+				deck.save().then(result => {
+					console.log(
+						'SoulCaptain saved the deck (and your soul).',
+						JSON.stringify(result, null, 2)
+					);
+					res.send('OK');
+				}).catch((err) => {
+					console.log('save error', err);
+					res.send('BAD\n' + JSON.stringify(err, null, 2));
+				});
+			}).catch(function pokaError(error) {
+				console.log('to je error z findOne', error);
+				res.send('BAD\n' + JSON.stringify(error, null, 2));
 			});
-		}).catch(function pokaError(error) { 
-			console.log('to je error z findOne', error);
-			res.send('BAD\n' + JSON.stringify(error, null, 2));
-		});
 
 	} // saveDeck
 
-	function createCard(req, res){
+	function createCard(req, res) {
 		let deckId = req.params.deckId;
 		console.log('body', req.body);
 		//req.body = require('../extras/scheme-souldeck.json').cards[0]; // TODO REMOVE;
@@ -119,18 +130,22 @@ module.exports = function (deps) {
 		let card = new Card(req.body); // req;
 		card.save().then((response) => {
 			console.log('created Card', response._id, response);
-			Deck.findOne({_id : deckId}).then(function _handleFoundDeck(foundDeck){
+			Deck.findOne({
+				_id: deckId
+			}).then(function _handleFoundDeck(foundDeck) {
 				foundDeck.cards.push(response._id);
-				foundDeck.save().then(function(result){
+				foundDeck.save().then(function (result) {
 					console.log('updated deck with new card', result);
-				}).catch(function _handleError(err){
+				}).catch(function _handleError(err) {
 					console.log('zapis się wysrał', err);
 				});
+			}).catch(err => {
+				console.log('createCard error in .then(function _handleFoundDeck(foundDeck)', err);
 			});
 		}).catch(err => {
-			console.log('createCard wyjebka', err);
+			console.log('createCard error in card.save().then((response)', err);
 		});
-		
+
 		console.log('deckId, card', deckId, card);
 		res.send('OK');
 	}
