3cebdf8168d59ab85dbdeceb705f8092f96c43b3
Wiktor <roktiw@gmail.com>
2018-07-12 21:21:51 +0200

    piękny refaktor

--- static/main.js
+++ static/main.js
@@ -145,21 +145,3 @@ $(function _onload() {
 	});
 
 }); // onload
-
-
-// Navbar js
-
-// el.classList.add(className); i tutaj 'el' to 'document.getElementById("leftNavbar")'
-function closeNav() {
-	document.getElementById('leftNavbar').classList.add('hideNavbarLeft');
-
-	document.getElementById('closeNavButton').classList.add('hideButton');
-	document.getElementById('openNavButton').classList.remove('hideButton');
-}
-
-function openNav() {
-	document.getElementById('leftNavbar').classList.remove('hideNavbarLeft');
-
-	document.getElementById('openNavButton').classList.add('hideButton');
-	document.getElementById('closeNavButton').classList.remove('hideButton');
-}
