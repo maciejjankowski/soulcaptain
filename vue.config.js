3f49c10c6858f50c40bd4cd8f40b57ff4c4033b7
mj <maj1337+git@gmail.com>
2018-07-24 22:23:43 +0200

    vue test

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  pwa: {
+    name: "Soulcaptain"
+  },
+
+  baseUrl: "/app/",
+  outputDir: "public/app",
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+};
